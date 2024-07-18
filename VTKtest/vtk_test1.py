cceea6218f4c74a6d4fe3544279452dff25172b0
LAlbertoA <luis.alberto@ciencias.unam.mx>
2024-07-18 14:52:08 -0600

    Added a new vtk test file (EP3DV50.vtk) and modified the vtk_test1.py file. Change the name of plot_press to slicer, added 3 arguments to control number of slices, place of slice, physical quantity to slice and dimension of slice.

--- VTKtest/vtk_test1.py
+++ VTKtest/vtk_test1.py
@@ -23,33 +23,65 @@ def print_mesh_info(m):
 def get_dims(m):
     return m.dimensions
 
-def plot_press(m, dims):
-    """ Plot pressure data  """
+def slicer(m, dims, parameters, slices, dimensions):
+    """
+    Function to plot slices of the parameters data given along any of the 3 axis at any length.
+    Currently only supporting 3D arrays from vtk files
+    Args:
+        m (pyvista.core.grid.ImageData): The array of data containing all the data and info about the simulation (output from 'load_mesh' function)
+        dims (tuple of int): Number of cells in each direction (output from get_dims function)
+        parameters (tuple of str): Name of data parameters to plot a slice of
+        slices (tuple of int): Lengths at which a slice plot will be made given in cell numbers NOT physical length
+        dimensions (tuple of str): Dimensions along a slice will be made. Valide options are: 'x', 'y', 'z'
+    Notes: The length of parameters, slices and dimensions tuples has to be the same.
+    """
+    if not len(parameters)==len(slices)==len(dimensions):
+        print(f"Error: Size of parameters, slices and dimensions arrays not equal")
+        sys.exit(1)
+        
     N3, N2, N1 = dims
     coords = m.points
-    x = coords[:, 0].reshape((N3, N2, N1))
+    num_of_plots = len(parameters)
+    plt_lay = 110*int(np.ceil(np.sqrt(num_of_plots)))+1
+    x = coords[:, 2].reshape((N3, N2, N1))
     y = coords[:, 1].reshape((N3, N2, N1))
-    z = coords[:, 2].reshape((N3, N2, N1))
-    rho = m.point_data['Pressure']
-    val = rho.reshape((N3, N2, N1))
-    print(val[:,:,:].shape)
-    #x[:, 0, :], y[:, 0, :],
-    plt.pcolormesh( val[:, :, 32], shading='gouraud', cmap='jet')
-    plt.gca().set_aspect('equal')
-    plt.colorbar()
+    z = coords[:, 0].reshape((N3, N2, N1))
+    fig = plt.figure()
+    for i in range(num_of_plots):
+        arr_to_plot = m.point_data[parameters[i]]
+        vals_to_plot = arr_to_plot.reshape((N3, N2, N1))
+        ax = plt.subplot(plt_lay+i)
+        if dimensions[i] == 'x':
+            pl = ax.pcolormesh(y[slices[i],:,slices[i]],z[slices[i],slices[i],:],vals_to_plot[slices[i],:,:],cmap='jet')
+        elif dimensions[i] == 'y':
+            pl = ax.pcolormesh(x[:,slices[i],slices[i]],z[slices[i],slices[i],:],vals_to_plot[:,slices[i],:],cmap='jet')
+        elif dimensions[i] == 'z':
+            pl = ax.pcolormesh(x[:,slices[i],slices[i]],y[slices[i],:,slices[i]],vals_to_plot[:,:,slices[i]],cmap='jet')
+        fig.colorbar(pl)
+        ax.set_title(parameters[i])
+    plt.tight_layout()
     plt.show()
-
+#    print(m.point_data)
+#    plt.pcolormesh(x[:,slc,slc], y[slc,:,slc],val[:, slc, :], cmap='jet')
+    #plt.gca().set_aspect('equal')
+#    plt.colorbar()
+#    plt.show()
+    
 def main():
     """ Main function to execute the script functionality. """
-    fp = 'NRotSphere003N100.vtk'
+    fp = 'EP3DV50.vtk'
 
     # Load and display mesh
     m = load_mesh(fp)
+    print(type(m.dimensions))
     print_mesh_info(m)
 
     # Plotting
     dims = get_dims(m)
-    plot_press(m, dims)
+    par = ['Density','Pressure','Density','Pressure']
+    dimen = ['x','x','x','x']
+    sly = [28,36,28,36]
+    slicer(m, dims,par,sly,dimen)
 
 if __name__ == "__main__":
     main()
